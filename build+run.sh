2ceb558ecedcd83010164a67296628022d380efc
atomtables <76572470+atomtables@users.noreply.github.com>
2024-04-10 13:20:03 -0400

    deleting more unnecessary files

--- build+run.sh
+++
@@ -1,2 +0,0 @@
-/opt/homebrew/bin/nasm -f bin /Users/atomtables/atom_asm/nasm_tutorial/boot.asm -o /Users/atomtables/atom_asm/nasm_tutorial/boot.bin
-/opt/homebrew/bin/qemu-system-x86_64 /Users/atomtables/atom_asm/nasm_tutorial/boot.bin 
